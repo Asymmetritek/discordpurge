98f765a6186adcb2ddf7babe8d65842e5d5f20c2
David Sanders <david.wm.sanders@gmail.com>
2019-12-28 08:24:09 +0000

    Refactor for better usability

--- discordpurge.py
+++ discordpurge.py
@@ -1,12 +1,15 @@
+"""discordpurge
+
+Usage:
+    discordpurge.py <target> <after_date>
+"""
 import datetime
 import sys
 from pathlib import Path
 
 import discord
+from docopt import docopt
 
-
-recipient_name = input("Target (username): ")
-after_date = datetime.datetime(2017, 9, 1)
 
 client = discord.Client()
 
@@ -20,8 +23,8 @@ async def on_ready():
         print("Acquiring DM channels...")
         dm_channels = (c for c in client.private_channels if isinstance(c, discord.DMChannel))
         # Get specific DM channel by recipient name
-        print(f"Finding specific DM channel with recipient '{recipient_name}'...")
-        dm_channel = list(c for c in dm_channels if c.recipient.name == recipient_name)[0]
+        print(f"Finding specific DM channel with recipient '{name}#{number}'...")
+        dm_channel = list(c for c in dm_channels if c.recipient.name == name and c.recipient.discriminator == number)[0]
         # Acquire message history from after specified date
         print(f"Deleting messages in history after {after_date}...")
         async for msg in dm_channel.history(limit=None, after=after_date):
@@ -35,13 +38,36 @@ async def on_ready():
         print(f"{str(e)}... Ctrl-C to quit!")
 
 
-token = None
-token_fp = Path(__file__).parent / Path("bot.token")
-with token_fp.open("r", encoding="utf-8") as f:
-    token = f.read()
-    token = token.replace("\n", "")
+if __name__ == '__main__':
+    args = docopt(__doc__)
 
-try:
-    client.run(token, bot=False)
-except KeyboardInterrupt:
-    print("Program quit by interrupt")
+    # Load the authentication token from auth_token.txt
+    token_fp = Path(__file__).parent / Path("auth_token.txt")
+    if token_fp.exists():
+        print(f"Loading token from '{token_fp.resolve()}'...")
+        with token_fp.open("r", encoding="utf-8") as f:
+            data = f.read()
+            token = data.replace("\n", "")
+    else:
+        print(f"Error: no 'auth_token.txt' file found in '{token_fp.parent.resolve()}'")
+        sys.exit(1)
+
+    # Parse the target argument into name and number variables
+    try:
+        name, number = args["<target>"].split("#")
+    except ValueError:
+        print("Error: target must be supplied as 'name#number'")
+        sys.exit(1)
+
+    # Parse the after_date argument into a datetime object
+    try:
+        after_date = datetime.datetime.strptime(args["<after_date>"], "%Y-%m-%d")
+    except ValueError:
+        print("Error: date required in yyyy-mm-dd (e.g. 2017-09-01) format")
+        sys.exit(1)
+
+    try:
+        client.run(token, bot=False)
+        pass
+    except KeyboardInterrupt:
+        print("Program quit by interrupt")
